44e2a59d2910b5e7765dfc0f587c1379c398bbcf
Jens Alfke <jens@couchbase.com>
2013-09-28 15:18:45 -0700

    Made Cover() macro faster
    Skip the function call when no test case is running.

--- Test.h
+++ Test.h
@@ -115,9 +115,11 @@ NSString* WhyUnequalObjects(id a, id b);
 #if DEBUG
 #define TestedBy(TEST_NAME) static const char* __unused kTestedBy = #TEST_NAME; \
             extern void Test_##TEST_NAME(void); __unused void* x = &Test_##TEST_NAME
-#define Cover(CONDITION) ({BOOL _b=!!(CONDITION); \
-                           _Cover(__FILE__, __LINE__, kTestedBy, #CONDITION, _b); \
-                           _b;})
+#define Cover(CONDITION) ({ \
+    BOOL _b=!!(CONDITION); \
+    if (__builtin_expect(gRunningTestCase,NO)) \
+        _Cover(__FILE__, __LINE__, kTestedBy, #CONDITION, _b); \
+    _b;})
 #else
 #define TestedBy(TEST_NAME)
 #define Cover(CONDITION) (CONDITION)
